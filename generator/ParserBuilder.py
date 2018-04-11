060a4b245f2012dd43a26c2e40470637406bbabc
Tomek Vivobook <tomasz.k.gac@gmail.com>
2018-04-11 20:26:47 +0200

    Zmiana implementacji ParserBuildera. Dodanie Recursion jako obiektu łączącego rekursję,

--- generator/ParserBuilder.py
+++ generator/ParserBuilder.py
@@ -4,25 +4,41 @@
 
 class ParserBuilder:
   def __init__( self, compiler ):
-    self._visited   = {}
     self.compiler  = compiler
+      # rule -> parser mapping
+      # None siginifies a recursive call to parent parser
+    self._visited   = {}
     
   def __call__( self, target ):
     name = type(target).__name__
-    
+      
+      # Test whether target has already been visited
     if target in self._visited:
-      return self._visited[target]  # Parser already built, return it
+      if self._visited[target] is None:
+          # Parser has already been visited, but has not yet been built
+          # Create a delayed redirection to be initialized after parent parser has been built
+        self._visited[target] = Recursion()
+      return self._visited[target]
     else:
       if not hasattr( self.compiler, name ):
-        raise RuntimeError("Compiler " + type(self.compiler).__name__ + " does not support building for type " + name )
+        raise RuntimeError("Compiler " + type(self.compiler).__name__ + " does not support building of type " + name )
     
-    self._visited[target] = None  # recursion guard
+    self._visited[target] = None  # recursion guard - parser not yet built
     child_parsers = tuple( self( child ) for child in target ) # build children parsers
     
-      # get attribute named as target class name from compiler
+      # compile method is named after class it is supposed to compile
     compile = getattr( self.compiler, name )
-      # call the builder compiler and pass target and child parsers as arguments
     parser = compile( target, child_parsers )
+          
+    if self._visited[target] is None: # No children referencing this parser
+      self._visited[target] = parser  # No recursion
+    elif isinstance( self._visited[target], Recursion ):  # A child references this parser
+      self._visited[target].target = parser  # update the recursion target
     
-    self._visited[target] = parser  # Save 
     return parser
+    
+class Recursion:
+  __slots__ = 'target'
+  
+  def __call__( self, executor ):
+    return self.target( executor )
--- generator/compiler.py
+++ generator/compiler.py
@@ -6,41 +6,39 @@ class RuleCompilerBase:
     self._terminals = terminals
     super().__init__()
   
-  def Terminal( self, target, child_methods ):
+  def Terminal( self, target, children ):
     try:
       return self._terminals[target.handle]
     except KeyError:
       raise RuntimeError('Missing terminal during compilation: '+str(target.handle))
     
-  def Handle( self, target, child_methods ):
-    def _Handle( evaluator ):
-      return child_methods[0]( evaluator )
-    return _Handle
+  def Handle( self, target, children ):
+    return children[0]
     
-  def Not( self, target, child_methods ):
+  def Not( self, target, children ):
     def _Not( evaluator ):
       try:
-        result = child_methods[0]( evaluator )
+        result = children[0]( evaluator )
       except ParserFailedException:
         return []
       
       raise ParserFailedException('Parser marched a Not statement')
     return _Not
     
-  def Optional( self, target, child_methods ):  
+  def Optional( self, target, children ):  
     def _Optional( evaluator ):
       save = evaluator.state.save()
       try:        
-        return child_methods[0]( evaluator )
+        return children[0]( evaluator )
       except ParserFailedException:
         evaluator.state.load( save )
         return []
     return _Optional
   
-  def Alternative( self, target, child_methods ):
+  def Alternative( self, target, children ):
     def _Alternative( evaluator ):
-      for rule in child_methods:
-        save = evaluator.state.save() # entry evaluator
+      for rule in children:
+        save = evaluator.state.save() # entry state
         try:
           return rule( evaluator )  # try visiting
         except ParserFailedException:
@@ -49,33 +47,32 @@ class RuleCompilerBase:
       raise ParserFailedException('Parser alternative exhausted with no match') # all options exhausted with no match
     return _Alternative
     
-  def Sequence( self, target, child_methods ):
+  def Sequence( self, target, children ):
     def _Sequence( evaluator ):
       sequence = [  ]
-      for rule in child_methods:
-          # Execute eagerly
-        sequence.extend( evaluator( rule( evaluator ) ) )
+      for rule in children:
+        sequence.extend( evaluator( rule( evaluator ) ) ) # Execute in order
          
       return sequence
     return _Sequence
     
-  def Repeat( self, target, child_methods ):
+  def Repeat( self, target, children ):
     def _Repeat( evaluator ):
       sequence = []
       save = None
       try:
         while True:
           save = evaluator.state.save() #save evaluator from before visitation
-          sequence.extend( child_methods[0]( evaluator ) )
+          sequence.extend( children[0]( evaluator ) )
       except ParserFailedException:
         evaluator.state.load( save )  # repeat until failure. Discard failed evaluator
         return sequence
     return _Repeat
     
-  def Push( self, target, child_methods ):
+  def Push( self, target, children ):
       # Do not execute
     def _Push( evaluator ):
-      result = child_methods[0]( evaluator )
+      result = children[0]( evaluator )
       return result
     return _Push
       
@@ -83,19 +80,19 @@ class Reordering( RuleCompilerBase ):
   def __init__( self, terminals ):
     super().__init__( terminals )
     
-  def Sequence( self, target, child_methods ):
+  def Sequence( self, target, children ):
       # Start executing after obtaining all of sequence
     def _Sequence( evaluator ):
       sequence = []
-      for rule in child_methods:
+      for rule in children:
         sequence.extend( rule( evaluator ) )
       
       return evaluator( sequence )
     
     return _Sequence   
     
-  def Push( self, target, child_methods ):      
+  def Push( self, target, children ):      
     def _Push( evaluator ):
-      result = child_methods[0]( evaluator )
+      result = children[0]( evaluator )
       return evaluator( result )
     return _Push
